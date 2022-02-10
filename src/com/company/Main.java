9483d9d04364a08a258c394ae509d43cb3815c12
Russell <russell_soper@hotmail.co.uk>
2022-02-10 16:38:40 +0000

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Book book1 =  new Book("David Barnes, Michael Kolling", "Objects first with Java","1234567");
         Library londonLibrary = new Library("London library");
-        londonLibrary.addBook(book1.getAuthor(),book1.getTitle(), book1.getId());
+        londonLibrary.addBook(book1.getAuthor(), book1.getTitle(), book1.getId());
         System.out.println(londonLibrary.getMatchingBooks("Objects first with Java"));
     }
 }
